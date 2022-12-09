c91084f6ba3c2895a9b2692ab9bb425837b90eef
porterehunley <porterehunley@gmail.com>
2022-12-09 12:42:48 -0800

    pagination

--- components/PageDeck.js
+++ components/PageDeck.js
@@ -10,17 +10,22 @@ import {
 	LinkBox,
 	Box,
 	Square,
-	Text
+	Text,
+	useBreakpointValue
 } from '@chakra-ui/react'
 
 
 export function PageButton(props) {
-	const { children, color, bg, onClick, ...kv } = props;
+	const { children, color, bg, fontWeight, borderColor, borderWidth, onClick, ...kv } = props;
 
 	return (
 		<Box pl={1} pr={1} {...kv}>
-			<Square h='100%' w='100%' bg={bg || 'black'} onClick={onClick}>
-				<Text color={color || 'white'}>{children}</Text>
+			<Square h='100%' w='100%' 
+				bg={bg || 'clear'} borderRadius={4} 
+				borderColor={borderColor}
+				borderWidth={borderWidth}
+				onClick={onClick}>
+				<Text fontSize={'14px'} fontWeight={fontWeight || '500'} color={color || 'black'}>{children}</Text>
 			</Square>
 		</Box>
 	);
@@ -28,6 +33,8 @@ export function PageButton(props) {
 
 export default function PageDeck(props) {
 	const { children, no_pages, page_no, onPageClick, ...kv } = props;
+
+	const noButtons = useBreakpointValue({ base: 4, lg: 6 });
 
 	let arrOne = children.filter((x,i) => i % 2 === 0);
 	let arrTwo = children.filter((x,i) => i % 2 != 0);
@@ -50,30 +57,39 @@ export default function PageDeck(props) {
 				{arrTwo.map(x => (x))}
 				</VStack>
 
-				<HStack w='100%' alignItems='stretch'>
-				<Button w='25%'>{'Previous'}</Button>
+				<HStack w='100%' alignItems='stretch' spacing={1}>
+				<Button variant={'outlined'} w='20%' m={0}>{'Prev'}</Button>
 
-				<Flex w='50%' alignItems='stretch'>
-				{[... new Array(no_pages).keys()].slice((page_no-1), page_no + 4)?.map((x, i) => {
-					if (i === 3 && no_pages >= 5) {
+				<Flex w='60%' alignItems='stretch'>
+				{[... new Array(no_pages).keys()].slice((page_no-1), (page_no-1) + noButtons)?.map((x, i) => {
+					if (i === (noButtons - 2) && no_pages >= noButtons) {
 						return (
-							<PageButton key={x+'-page-button'} onClick={onPageClick} w={`${100/5}%`}>{'...'}</PageButton>
+							<PageButton key={x+'-page-button'}
+								onClick={onPageClick}
+								w={`${100/noButtons}%`}>{'...'}</PageButton>
 						);
 					}
 
-					if (i === 4 && no_pages >= 5) {
+					if (i === (noButtons - 1) && no_pages >= noButtons) {
 						return (
-							<PageButton key={x+'-page-button'} onClick={() => onPageClick(no_pages)} w={`${100/5}%`}>{no_pages}</PageButton>
+							<PageButton key={x+'-page-button'}
+								onClick={() => onPageClick(no_pages)}
+								w={`${100/noButtons}%`}>{no_pages}</PageButton>
 						);
 					}
 
 					return (
-						<PageButton key={x+'-page-button'} onClick={() => onPageClick(x+1)} w={`${100/5}%`}>{x + 1}</PageButton>
+						<PageButton key={x+'-page-button'}
+							borderColor={page_no === x + 1 ? 'purple.400' : 'none'}
+							borderWidth={page_no === x + 1 ? '1px' : '0px'}
+							fontWeight={page_no === x + 1 ? '800' : '500'}
+							onClick={() => onPageClick(x+1)}
+							w={`${100/noButtons}%`}>{x + 1}</PageButton>
 					);
 				})}
 				</Flex>
 
-				<Button w='25%'>{'Next'}</Button>
+				<Button w='20%' m={0}>{'Next'}</Button>
 				</HStack>
 
 			</Flex>
