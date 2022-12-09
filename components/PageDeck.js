593db68e95c3df5c53acdcde4190257b41c4df41
porterehunley <porterehunley@gmail.com>
2022-12-09 08:43:14 -0800

    init pageable deck

--- components/PageDeck.js
+++ components/PageDeck.js
@@ -4,13 +4,30 @@ import {
 	HStack,
 	Skeleton,
 	VStack,
+	Link,
+	LinkOverlay,
+	Button,
+	LinkBox,
 	Box,
+	Square,
 	Text
 } from '@chakra-ui/react'
 
 
+export function PageButton(props) {
+	const { children, color, bg, onClick, ...kv } = props;
+
+	return (
+		<Box pl={1} pr={1} {...kv}>
+			<Square h='100%' w='100%' bg={bg || 'black'} onClick={onClick}>
+				<Text color={color || 'white'}>{children}</Text>
+			</Square>
+		</Box>
+	);
+}
+
 export default function PageDeck(props) {
-	const { children, no_pages, onPageClick, ...kv } = props;
+	const { children, no_pages, page_no, onPageClick, ...kv } = props;
 
 	let arrOne = children.filter((x,i) => i % 2 === 0);
 	let arrTwo = children.filter((x,i) => i % 2 != 0);
@@ -21,8 +38,7 @@ export default function PageDeck(props) {
 	return (
 		<Flex 
 			direction='column'
-			h='100%'
-			w='100%'
+			h='100%' w='100%'
 			overflow='scroll' 
 			{...kv}>
 			<Flex w='100%' gap={[5, 2]} flexDirection={['column','row']}>
@@ -33,6 +49,33 @@ export default function PageDeck(props) {
 				<VStack w='100%' spacing={[5, 2]}>
 				{arrTwo.map(x => (x))}
 				</VStack>
+
+				<HStack w='100%' alignItems='stretch'>
+				<Button w='25%'>{'Previous'}</Button>
+
+				<Flex w='50%' alignItems='stretch'>
+				{[... new Array(no_pages).keys()].slice((page_no-1), page_no + 4)?.map((x, i) => {
+					if (i === 3 && no_pages >= 5) {
+						return (
+							<PageButton key={x+'-page-button'} onClick={onPageClick} w={`${100/5}%`}>{'...'}</PageButton>
+						);
+					}
+
+					if (i === 4 && no_pages >= 5) {
+						return (
+							<PageButton key={x+'-page-button'} onClick={() => onPageClick(no_pages)} w={`${100/5}%`}>{no_pages}</PageButton>
+						);
+					}
+
+					return (
+						<PageButton key={x+'-page-button'} onClick={() => onPageClick(x+1)} w={`${100/5}%`}>{x + 1}</PageButton>
+					);
+				})}
+				</Flex>
+
+				<Button w='25%'>{'Next'}</Button>
+				</HStack>
+
 			</Flex>
 		</Flex>
 	);
--- components/StudiesLandingDeck.js
+++ components/StudiesLandingDeck.js
@@ -10,32 +10,40 @@ import studyHttpClient from '../services/clientapis/StudyHttpClient';
 
 export default function StudiesLandingDeck() {
 	const [studies, setStudies] = useState([]);
+	const [noPages, setNoPages] = useState(undefined);
 
 	const router = useRouter();
 
-	const { treatments, conditions, q, gender } = router.query;
+	const { treatments, conditions, q, gender, page } = router.query;
 
 	useEffect(() => {
 		fetchStudiesFromQuery();
-	}, [conditions, treatments, q])
+	}, [conditions, treatments, q, page])
 
 	async function fetchStudiesFromQuery(hardRefresh=false) {
 		const params = {
 			'q': q,
 			'conditions': conditions,
 			'treatments': treatments,
-			'gender': gender
+			'gender': gender,
 		}
 
-		studyHttpClient.search(1, params).then(data => {
+		studyHttpClient.search(page || 1, params).then(data => {
 			let studiesToAdd = data['studies'];
+
 			setStudies(studiesToAdd);
+			setNoPages(Math.ceil(data['total'] / 10))
 		})
+	}
+
+	function onPageClick(page) {
+		router.query.page = page;
+		router.push(router, undefined, { shallow: true });
 	}
 
 	return (
 		<Flex flexDirection='column'>
-			<PageDeck p={0}>
+			<PageDeck p={0} no_pages={noPages} page_no={parseInt(page) || 1} onPageClick={onPageClick}>
 			{studies?.map(x => (<StudyCard w={['100%', '100%']} key={x.id} {...x}/>))}
 			</PageDeck>
 		</Flex>
